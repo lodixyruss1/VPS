beac5e23d394602049b079d7bd65840e1bb010a8
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-04-01 21:59:16 +0800

    Create lllimitless-vpn-coinsmanager.sh

---
+++ lllimitless-vpn-coinsmanager.sh
@@ -0,0 +1,29 @@
+{
+
+"Version":"1",
+
+"AdminCoin":"lodixyrussdevz2021s",
+
+"Codes":[
+
+{
+
+"Name":"Code 1",
+
+"Code":"SHARE_LIMITLESSVPN",
+
+"Coins":20
+
+},
+
+{
+
+"Name":"Code 2",
+
+"Code":"LIMITLESSVPN",
+
+"Coins":10
+
+}
+
+]}
