634d8058de3256c0fa3d57f9758d94e53f053263
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-04-13 12:33:50 +0800

    Update lllimitlessvpn-update-app.sh

--- lllimitlessvpn-update-app.sh
+++ lllimitlessvpn-update-app.sh
@@ -7,11 +7,11 @@
 • Install the new version in PlayStore this version is temporary unavailable.<br>
 • Reason of application update merging of Ads only i used my old Ads platform in order to maintain the free service of limitlessvpn<br>
 • Update your application now. Support Limitless VPN always, your support is highly appreciated<br><br>
-• What's new:
-• Added buildin server account limitless
-• Redeem coins management is updated
-• System UI enhance
-• Fixed minimal bug
+• What's new:<br>
+• Added buildin server account limitless<br>
+• Redeem coins management is updated<br>
+• System UI enhance<br>
+• Fixed minimal bug<br><br>
 • like our fb page: https://facebook.com/limitlessvpn2022<br>
 • Supported Android 5 & above<br>
 
