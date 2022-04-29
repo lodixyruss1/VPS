42eacd6ba48e72df4a70aa0883a11e725b3b2ba2
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-04-29 09:49:32 +0800

    Update lllimitlessvpn-update-app2.sh

--- lllimitlessvpn-update-app2.sh
+++ lllimitlessvpn-update-app2.sh
@@ -1,10 +1,11 @@
 {
-"newVersion": "9.8",
+"newVersion": "10.8",
 "apkUrl": "https://play.google.com/store/apps/details?id=itunnelssh.vpn.limitless",
 "versionNotes":"
 •Limitless VPN•<br>
 <br>
 • Fixed android 12<br>
+• Fixed crashed issue<br>
 • Server flag update<br>
 • Optimize UI<br>
 • Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
