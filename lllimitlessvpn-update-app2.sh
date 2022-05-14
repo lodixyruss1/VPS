625f0e75ad8ae3cc260240018da36d45f05ba326
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-05-14 19:20:41 +0800

    Update lllimitlessvpn-update-app2.sh

--- lllimitlessvpn-update-app2.sh
+++ lllimitlessvpn-update-app2.sh
@@ -1,15 +1,15 @@
 {
-"newVersion": "12.8",
-"apkUrl": "https://play.google.com/store/apps/details?id=itunnelssh.vpn.limitless",
+"newVersion": "8.8",
+"apkUrl": "https://play.google.com/store/apps/details?id=xyruss.vpn.limitless",
 "versionNotes":"
 •Limitless VPN•<br>
 <br>
-• Custom Import & Export Finally Fixed.<br>
-• You Can Now Custom Setup and Export Your Own Config and Share to others.<br>
-
-
+• New Limitless VPN App<br>
+• New Limitless VPN Ads<br>
+• Download This New App<br>
+• Old Limitless VPN will  be down soon<br>
+• Don't forget to claim 50Coins for startup<br>
 • Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
-
 • Supported Android 5 & above<br>
 "
 }
