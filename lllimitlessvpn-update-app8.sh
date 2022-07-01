e42663768d8a436b60a08a6e2ef25dce2d627382
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-07-01 23:13:04 +0800

    Update lllimitlessvpn-update-app8.sh

--- lllimitlessvpn-update-app8.sh
+++ lllimitlessvpn-update-app8.sh
@@ -1,1 +1,23 @@
+{
 
+"newVersion": "11.8",
+
+"apkUrl": "https://play.google.com/store/apps/details?id=xyruss.vpn.limitless",
+
+"versionNotes":"
+
+•Limitless VPN•<br>
+
+<br>
+
+• Ads replacement temporary<br>
+
+• Fixed ads<br>
+
+• Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
+
+• Supported Android 5 & above<br>
+
+"
+
+}
