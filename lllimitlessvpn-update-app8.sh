ce0f6aa6ac220500eef6855383a2ceb28bd25324
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-07-11 11:00:19 +0800

    Update lllimitlessvpn-update-app8.sh

--- lllimitlessvpn-update-app8.sh
+++ lllimitlessvpn-update-app8.sh
@@ -1,6 +1,6 @@
 {
 
-"newVersion": "12.8",
+"newVersion": "14.8",
 
 "apkUrl": "https://play.google.com/store/apps/details?id=xhypher.prine.limitlessvpn",
 
@@ -9,12 +9,12 @@
 •Limitless VPN - Xhypher Prine Edition•<br>
 
 <br>
-• Please install this new application version (Previous Limitless VPN will be show down soon)<br>
-• Enhanced UI / Light and Dark<br>
-• Ads replacement (Fixed) in order to run the service, ads is important<br>
 
-• SSH Service Update <br>
-• Added Injector Service Running Background<br>
+
+• Fixed Android 12<br>
+
+• Fixed Android 13-beta <br>
+• Fixed Injector Service Running Background<br>
 • Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
 
 • Supported Android 5 & above<br>
