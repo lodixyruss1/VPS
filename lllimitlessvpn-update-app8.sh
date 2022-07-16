e18b1965ae1ebb2d27bf2e35f2931cadafbe6958
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-07-17 07:57:26 +0800

    Update lllimitlessvpn-update-app8.sh

--- lllimitlessvpn-update-app8.sh
+++ lllimitlessvpn-update-app8.sh
@@ -11,10 +11,10 @@
 <br>
 
 
-• Fixed Android 12<br>
+• Ads replace temporary<br>
 
-• Fixed Android 13-beta <br>
-• Fixed Injector Service Running Background<br>
+
+
 • Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
 
 • Supported Android 5 & above<br>
