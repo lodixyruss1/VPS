c16bad27dca963c3135d22374afc71ee5e921d01
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-07-26 15:19:38 +0800

    Update lllimitlessvpn-update-app8.sh

--- lllimitlessvpn-update-app8.sh
+++ lllimitlessvpn-update-app8.sh
@@ -1,24 +1,16 @@
 {
-
-"newVersion": "15.8",
-
-"apkUrl": "https://play.google.com/store/apps/details?id=xhypher.prine.limitlessvpn",
-
+"newVersion": "13.8",
+"apkUrl": "https://play.google.com/store/apps/details?id=itunnelssh.vpn.limitless",
 "versionNotes":"
-
-•Limitless VPN - Xhypher Prine Edition•<br>
-
+•Limitless VPN•<br>
 <br>
-
-
-• Ads replace temporary<br>
-
-
-
-• Like our fb page: https://www.facebook.com/limitlessvpn2022<br>
-
+• Install the new version in PlayStore this version is temporary unavailable.<br>
+• Reason of application update merging of Ads only i used my old Ads platform in order to maintain the free service of limitlessvpn<br>
+• Update your application now. Support Limitless VPN always, your support is highly appreciated<br><br>
+• What's new:<br>
+• Added cloud messaging for future updates<br>
+• Fixed Ads<br><br>
+• like our fb page: https://facebook.com/limitlessvpn2022<br>
 • Supported Android 5 & above<br>
-
 "
-
 }
