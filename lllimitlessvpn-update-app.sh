eb692f1ab56545f5853b9da7f0c2e93d5cd4978b
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2022-07-28 14:56:44 +0800

    Update lllimitlessvpn-update-app.sh

--- lllimitlessvpn-update-app.sh
+++ lllimitlessvpn-update-app.sh
@@ -1,16 +1,13 @@
 {
-"newVersion": "14.8",
+"newVersion": "15.8",
 "apkUrl": "https://play.google.com/store/apps/details?id=itunnelssh.vpn.limitless",
 "versionNotes":"
 •Limitless VPN•<br>
 <br>
-• Install the new version in PlayStore this version is temporary unavailable.<br>
-• Reason of application update merging of Ads only i used my old Ads platform in order to maintain the free service of limitlessvpn<br>
-• Update your application now. Support Limitless VPN always, your support is highly appreciated<br><br>
-• What's new:<br>
-• Added buildin server account limitless<br>
-• Redeem coins management is updated<br>
-• System UI enhance<br>
+
+
+• Fixed IP Hunt Crash<br>
+• Added firebase messaging<br>
 • Fixed minimal bug<br><br>
 • like our fb page: https://facebook.com/limitlessvpn2022<br>
 • Supported Android 5 & above<br>
