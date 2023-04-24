a2a83de7d3106ebf6f580d8c1b76502990a04da3
xxSTOPERSxx VPN <43051930+lodixyruss1@users.noreply.github.com>
2023-04-24 11:17:48 +0800

    Create rev.lllxhyphertunnel-coin.sh

---
+++ rev.lllxhyphertunnel-coin.sh
@@ -0,0 +1,57 @@
+{
+
+"Version":"1",
+
+"AdminCoin":"lodixyrussdevz2021s",
+
+"Codes":[
+{
+
+"Name":"Code 1",
+
+"Code":"NETH GAMAY OTIN",
+
+"Coins":5
+
+},
+{
+
+"Name":"Code 2",
+
+"Code":"NETH PESOT",
+
+"Coins":10
+
+},
+
+{
+
+"Name":"Code 3",
+
+"Code":"XHYPHERTUNNEL",
+
+"Coins":20
+
+},
+
+{
+
+"Name":"Code 4",
+
+"Code":"ILOVE-XHYPHERTUNNEL",
+
+"Coins":15
+
+},
+
+{
+
+"Name":"Code 5",
+
+"Code":"5COINS",
+
+"Coins":5
+
+}
+
+]}
